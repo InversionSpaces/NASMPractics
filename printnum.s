d91454cc2f8c34450f4866f545df2db7a211fe82
InversionSpaces <InversionSpaces@vivaldi.net>
2020-03-15 22:48:33 +0300

    printnum

---
+++ printnum.s
@@ -0,0 +1,40 @@
+	global	printnum
+	
+	section	.text
+
+;===================
+; Inputs:	RAX - number to print
+;		RBX - base
+; Outputs:	number in base to stdout
+; Destructs:	
+;===================
+printnum:
+	mov	rsi, buffer + bufsize
+
+convert:
+; convert {
+	dec	rsi
+
+	xor	rdx, rdx		; rdx = 0
+	div	rbx			; rdx = r, rax = q
+	
+	mov	dl, [dict + rdx]
+	mov	[rsi], dl
+
+	cmp	rax, 0h
+	jne	convert
+; } convert
+
+	mov	rdx, buffer + bufsize
+	sub	rdx, rsi
+
+	mov	rax, 1h
+	mov	rdi, 1h
+	
+	syscall
+
+	ret
+	section	.data
+dict:	db '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+buffer:	times 64 db 0
+bufsize	equ $ - buffer
